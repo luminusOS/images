88c0e927b5da6835ade6600d89576fe2fc95661d
Leandro Rodrigues <leandromqrs@hotmail.com>
2026-06-05 15:51:03 -0300

    fix(finalize): use --no-autoremove when removing rpm-ostree to prevent side effects

--- common/finalize.sh
+++ common/finalize.sh
@@ -3,6 +3,10 @@ set -uexo pipefail
 
 # Final cleanup and commit
 
+if rpm -q rpm-ostree >/dev/null 2>&1; then
+  dnf -y remove --no-autoremove rpm-ostree
+fi
+
 dnf clean all
 rm -rf /var/cache/dnf /var/cache/libdnf5 /tmp/*
 
